5b10f75370efce2d1a876f31680f05e82c50da43
theobanette <theobanette@hotmail.fr>
2025-12-28 01:00:16 +0100

    vérification du statut de l'amitié à ACCEPTED avant de la supprimer

--- src/main/java/com/calendar/social/infrastucture/repositories/RelationshipRepository.java
+++ src/main/java/com/calendar/social/infrastucture/repositories/RelationshipRepository.java
@@ -8,7 +8,9 @@ import reactor.core.publisher.Mono;
 public interface RelationshipRepository extends ReactiveNeo4jRepository<RelationshipEntity, Long> {
 
     @Query("MATCH (me:User {userId: $myId})-[r:RELATIONSHIP]-(other:User {userId: $otherId}) " +
+            "WHERE r.status = 'ACCEPTED' " +
+            "WITH r, r AS deletedRel " +
             "DELETE r " +
-            "RETURN r")
+            "RETURN deletedRel")
     Mono<RelationshipEntity> deleteFriendship(Long myId, Long otherId);
 }
