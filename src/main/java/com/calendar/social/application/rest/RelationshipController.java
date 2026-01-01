80b3241e8eac839b8287ff4c4214bbaa17252e3f
theobanette <theobanette@hotmail.fr>
2026-01-01 22:03:51 +0100

    Ajout de la validation sur les entrées utilisateur et amélioration des réponses HTTP pour les contrôleurs User et Relationship, ainsi que gestion des exceptions de validation.

--- src/main/java/com/calendar/social/application/rest/RelationshipController.java
+++ src/main/java/com/calendar/social/application/rest/RelationshipController.java
@@ -4,12 +4,17 @@ import com.calendar.social.application.rest.dtos.FriendRequestDTO;
 import com.calendar.social.domain.models.UserNodeDTO;
 import com.calendar.social.domain.services.RelationshipService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("relationships")
+@Validated
 public class RelationshipController {
 
     private final RelationshipService relationshipService;
@@ -19,22 +24,26 @@ public class RelationshipController {
     }
 
     @PostMapping("request")
-    public Mono<ResponseEntity<UserNodeDTO>> sendFriendRequest(@RequestHeader("X-Internal-User-Id") Long userId, @RequestBody @Valid FriendRequestDTO friendRequestDTO) {
-        return relationshipService.sendFriendRequest(userId, friendRequestDTO.userTag()).map(ResponseEntity::ok);
+    public Mono<ResponseEntity<UserNodeDTO>> sendFriendRequest(@NotBlank @RequestHeader("X-Internal-User-Id") Long userId, @RequestBody @Valid FriendRequestDTO friendRequestDTO) {
+        return relationshipService.sendFriendRequest(userId, friendRequestDTO.userTag())
+                .flatMap(userNodeDTO -> Mono.just(ResponseEntity.created(URI.create("relationships/request")).body(userNodeDTO)));
     }
 
     @PutMapping("accept/{senderId}")
-    public Mono<ResponseEntity<UserNodeDTO>> acceptFriendRequest(@RequestHeader("X-Internal-User-Id") Long userId, @PathVariable Long senderId) {
-        return relationshipService.acceptFriendRequest(userId, senderId).map(ResponseEntity::ok);
+    public Mono<ResponseEntity<Void>> acceptFriendRequest(@NotBlank @RequestHeader("X-Internal-User-Id") Long userId, @NotBlank @PathVariable Long senderId) {
+        return relationshipService.acceptFriendRequest(userId, senderId)
+                .thenReturn(ResponseEntity.noContent().build());
     }
 
     @PutMapping("reject/{senderId}")
-    public Mono<ResponseEntity<UserNodeDTO>> rejectFriendRequest(@RequestHeader("X-Internal-User-Id") Long userId, @PathVariable Long senderId) {
-        return relationshipService.rejectFriendRequest(userId, senderId).map(ResponseEntity::ok);
+    public Mono<ResponseEntity<Void>> rejectFriendRequest(@NotBlank @RequestHeader("X-Internal-User-Id") Long userId, @NotBlank @PathVariable Long senderId) {
+        return relationshipService.rejectFriendRequest(userId, senderId)
+                .thenReturn(ResponseEntity.noContent().build());
     }
 
     @DeleteMapping("{friendId}")
-    public Mono<Void> deleteFriendship(@RequestHeader("X-Internal-User-Id") Long userId, @PathVariable Long friendId) {
-        return relationshipService.deleteFriendship(userId, friendId);
+    public Mono<ResponseEntity<Void>> deleteFriendship(@NotBlank @RequestHeader("X-Internal-User-Id") Long userId, @NotBlank @PathVariable Long friendId) {
+        return relationshipService.deleteFriendship(userId, friendId)
+                .thenReturn(ResponseEntity.noContent().build());
     }
 }
--- src/main/java/com/calendar/social/application/rest/UserController.java
+++ src/main/java/com/calendar/social/application/rest/UserController.java
@@ -2,11 +2,15 @@ package com.calendar.social.application.rest;
 
 import com.calendar.social.domain.models.UserResult;
 import com.calendar.social.domain.services.UserService;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("users")
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -16,9 +20,10 @@ public class UserController {
     }
 
     @GetMapping
-    public Flux<UserResult> readAllWithSocialStatus(
-            @RequestHeader("X-Internal-User-Id") Long userId,
+    public ResponseEntity<Flux<UserResult>> readAllWithSocialStatus(
+            @NotBlank @RequestHeader("X-Internal-User-Id") Long userId,
             @RequestParam(required = false) String friendshipStatus) {
-        return userService.readAllWithSocialStatus(userId, friendshipStatus);
+        return ResponseEntity.ok()
+                .body(userService.readAllWithSocialStatus(userId, friendshipStatus));
     }
 }
