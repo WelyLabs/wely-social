b24c5ba3dadb58b80c94a1d2c0adb43995318ad0
theobanette <theobanette@hotmail.fr>
2026-01-01 21:20:37 +0100

    fin d'implémentation de la gestion d'erreurs

--- src/main/java/com/calendar/social/exception/TechnicalException.java
+++ src/main/java/com/calendar/social/exception/TechnicalException.java
@@ -1,5 +1,8 @@
 package com.calendar.social.exception;
 
+import lombok.Getter;
+
+@Getter
 public class TechnicalException extends RuntimeException {
 
     private final TechnicalErrorCode errorCode;
